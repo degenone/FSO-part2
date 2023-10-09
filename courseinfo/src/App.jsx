99129596454ccebc9c23df3cdcbb0a9f2b3835ff
Tero Kilpeläinen <degenone1@gmail.com>
2023-10-09 14:49:59 +0300

    exercise 2.1 done

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,4 +1,38 @@
-import { useState } from 'react';
+const Part = (props) => {
+    const { part } = props;
+    return (
+        <p>
+            {part.name} &mdash; {part.exercises}
+        </p>
+    );
+};
+
+
+const Content = (props) => {
+    const { course } = props;
+    return (
+        <>
+            {course.parts.map((part) => (
+                <Part key={part.id} part={part} />
+            ))}
+        </>
+    );
+};
+
+const Header = (props) => {
+    const { course } = props;
+    return <h1>{course.name}</h1>;
+};
+
+const Course = (props) => {
+    const { course } = props;
+    return (
+        <div>
+            <Header course={course} />
+            <Content course={course} />
+        </div>
+    );
+};
 
 const App = () => {
     const course = {
